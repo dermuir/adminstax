21db99fb2143a4e519f4c20e772dce57f9c8c56d
Dermuir <dermuir@hotmail.com>
2020-03-19 19:44:03 -0600

    version without mysql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  NbMenuService,
+  NbSidebarService,
+  NbSearchService,
+  NbSidebarModule,
+  NbMenuItem,
+  NbIconLibraries,
+  NbGetters,
+  NbTreeGridDataSource,
+  NbTreeGridDataSourceBuilder, NbSortDirection, NbSortRequest
+} from '@nebular/theme';
+
+interface TreeNode<T> {
+  data: T;
+  children?: TreeNode<T>[];
+  expanded?: boolean;
+}
+
+interface FSEntry {
+  name: string;
+  size: string;
+  kind: string;
+  items?: number;
+  childEntries?: FSEntry[];
+  expanded?: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +33,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adminstax';
+  show: boolean;
+   constructor(private sidebarService: NbSidebarService, private dataSourceBuilder: NbTreeGridDataSourceBuilder<FSEntry>) {
+     this.dataSource = this.dataSourceBuilder.create(this.data);
+     this.show = false;
+  }
+  customColumn = 'name';
+  defaultColumns = [ 'size', 'kind', 'items' ];
+  allColumns = [ this.customColumn, ...this.defaultColumns ];
+  source: NbTreeGridDataSource<FSEntry>;
+  dataSource: NbTreeGridDataSource<FSEntry>;
+
+  sortColumn: string;
+  sortDirection: NbSortDirection = NbSortDirection.NONE;
+
+  private data: TreeNode<FSEntry>[] = [
+    {
+      data: { name: 'Projects', size: '1.8 MB', items: 5, kind: 'dir' }
+    },
+    {
+      data: { name: 'Reports', kind: 'dir', size: '400 KB', items: 2 }
+    },
+    {
+      data: { name: 'Other', kind: 'dir', size: '109 MB', items: 2 }
+    },
+  ];
+
+
+  getShowOn(index: number) {
+    const minWithForMultipleColumns = 400;
+    const nextColumnStep = 100;
+    return minWithForMultipleColumns + (nextColumnStep * index);
+  }
+  updateSort(sortRequest: NbSortRequest): void {
+    this.sortColumn = sortRequest.column;
+    this.sortDirection = sortRequest.direction;
+  }
+
+  getSortDirection(column: string): NbSortDirection {
+    if (this.sortColumn === column) {
+      return this.sortDirection;
+    }
+    return NbSortDirection.NONE;
+  }
+  toggle() {
+    this.sidebarService.toggle(true);
+    return false;
+  }
+
+  search() {
+          this.show = !this.show;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbActionsModule,
+  NbSidebarModule,
+  NbTreeGridModule,
+  NbCardModule,
+  NbButtonModule
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule } from '@nebular/theme';
+import { BancoComponent } from './banco/banco.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AlmacenComponent } from './almacen/almacen.component';
+
+const appRoutes: Routes = [
+  { path: 'banco', component: BancoComponent },
+  { path: 'almacen', component: AlmacenComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BancoComponent,
+    AlmacenComponent
   ],
   imports: [
+    NbThemeModule.forRoot({name: 'dark'}),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbSidebarModule.forRoot(),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    NbActionsModule,
+    NbActionsModule,
+    NbTreeGridModule,
+    NbCardModule,
+    NbButtonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
